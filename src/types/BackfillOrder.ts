b0620fc367b1b62c12788d2dbbb0907b7a2a1619
Ashay Parikh <ashayparikh22@gmail.com>
2024-07-12 21:07:21 -0500

    Update package.json

--- src/types/BackfillOrder.ts
+++ src/types/BackfillOrder.ts
@@ -4,7 +4,7 @@ export const BackfillOrder = {
   // Processed all unprocessed blocks in a one-by-one order
   ASCENDING: "ASCENDING",
   DESCENDING: "DESCENDING",
-  // Process all unprocessed blocks in a paralleized manner with threads or promises
+  // Process all unprocessed blocks in a parallel order
   PARALLEL: "PARALLEL",
   // Only processess specified blocks
   SPECIFIC: "SPECIFIC",
--- workers/backfillBlockRange.ts
+++
@@ -1,5 +0,0 @@
-import { expose } from "threads/worker";
-import backfillBlockRange from "../utils/backfillBlockRange";
-
-// TODO: Make sure of multithreading for backfiller
-expose(backfillBlockRange);
